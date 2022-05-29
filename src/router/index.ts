60cfb8abbe8dcaab46b6beae34a7b859f46bb75d
JengYoung <young_pallete@naver.com>
2022-05-29 11:52:43 +0900

    :sparkles: :sparkles: add Route navigation for preventing requiredAuth Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,20 +6,46 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'home',
     component: HomeView,
+    meta: {
+      isRequired: true,
+    },
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/sign-in',
+    name: 'SignIn',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/SignIn.vue'),
+    meta: {
+      isRequired: false,
+    },
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/SignUp.vue'),
+    meta: {
+      isRequired: false,
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+// eslint-disable-next-line consistent-return
+router.beforeEach(async (to) => {
+  const token = window.localStorage.getItem('token');
+
+  if (to.meta.isRequired && !token) {
+    return { name: 'SignIn' };
+  }
 });
 
 export default router;
