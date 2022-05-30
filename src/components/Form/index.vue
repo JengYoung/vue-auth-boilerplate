47b2a18331d689babf68116b470c4e1420ce0668
JengYoung <young_pallete@naver.com>
2022-05-30 23:25:35 +0900

    :sparkles: Form 컴포넌트 구현 및 레이아웃 설정

---
+++ src/components/Form/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <form class="form">
+    <slot></slot>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FormContainer',
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+
+  max-width: 500px;
+  width: 100%;
+
+  padding: 50px;
+
+  border: 1px solid lightgray;
+  border-radius: 20px;
+
+  box-sizing: border-box;
+}
+</style>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="sign-in">
-    <h1>Login Page</h1>
-
-    <form action="" class="form">
+    <Form>
+      <h1>로그인</h1>
       <!-- eslint-disable vuejs-accessibility/form-control-has-label  -->
-      <input type="text" class="form__input" id="id">
-      <input type="text" class="form__input" id="password">
+      <label class="form__label" for="id">
+        <span>아이디</span>
+        <input type="text" class="form__input" id="id">
+      </label>
+      <label class="form__label" for="id">
+        <span>비밀번호</span>
+        <input type="text" class="form__input" id="password">
+      </label>
 
-      <button>로그인하기</button>
-    </form>
+      <button type="submit">로그인하기</button>
+    </Form>
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Form from '@/components/Form/index.vue';
+
+export default defineComponent({
+  components: {
+    Form,
+  },
+});
+</script>
 <style lang="scss" scoped>
 
 .sign-in {
@@ -19,11 +34,30 @@
   flex-direction: column;
   align-items: center;
 
-  .form {
+  .form__label {
     display: flex;
     flex-direction: column;
-    &__input {
+    margin-bottom: 1rem;
+
+    span {
+      margin-bottom: 0.5rem;
+      font-weight: 500;
+    }
+
+    .form__input {
+      height: 40px;
       margin-bottom: 1rem;
+
+      border: none;
+      border-bottom: 1px solid lightgray;
+
+      outline: none;
+      font-size: 1.125rem;
+
+      &:focus {
+        border-bottom: 1px solid #fcc6d2;
+        transition: all 0.3s;
+      }
     }
   }
 }
